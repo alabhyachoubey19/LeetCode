305a866a45b99b4bcadfb109b94e837837f15dcc
Alabhya Choubey <70913225+alabhyachoubey19@users.noreply.github.com>
2026-09-24 01:48:04 +0530

    Time: 0 ms (100%), Space: 26.3 MB (8.68%) - LeetHub

---
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int singleNonDuplicate(vector<int>& nums) {
+        if (nums.size() == 1) {
+            return nums[0];
+        }
+        if (nums[0] != nums[1]) {
+            return nums[0];
+        } // edge case 1
+        if (nums[nums.size() - 1] != nums[nums.size() - 2]) {
+            return nums[nums.size() - 1];
+        } // edge case 2
+
+        // binary search
+        int low = 1, high = nums.size() - 2;
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            if (nums[mid] != nums[mid + 1] && nums[mid] != nums[mid - 1]) {
+                return nums[mid];
+            }
+            if ((mid % 2 == 1 && nums[mid - 1] == nums[mid]) ||
+                (mid % 2 == 0 && nums[mid] == nums[mid + 1])) {
+                low = mid + 1; // eliminate the left half
+            } else {
+                high = mid - 1;
+            }
+        }
+        return -1;
+    }
+};
